f963f1845e0f05240f25a530c2397e8202df2e12
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 22:45:19 +0700

    Second Commit

---
+++ 9.js
@@ -0,0 +1,20 @@
+//Operator Aritmatika
+
+//membuat variabel dengan data numerik dan string
+let a = 2, b = 5, c = "10"
+
+//pertambahan data a dan b
+console.log("a + b = " + (a+b));
+/*pertambahan data a dan c 
+(data c string, maka bersifat tetap dan tidak dapat dioperasikan)*/
+console.log("a + c = " + (a+c));
+//pengurangan data a dikurangi data b
+console.log("a - b = " + (a-b));
+//perkalian data a dan b
+console.log("a * b = " + (a*b));
+//pembagian data b dengan data a
+console.log("b / a = " + (b/a));
+//menampilkan sisa pembagian dari data b dengan data a
+console.log("b % a = " + (b%a));
+//menampilkan hasil data b dipangkat data a
+console.log("b ** a = " + (b**a));
